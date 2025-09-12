c0247b5d148799e20fa7406f5a52cb5e106ad71e
Sabri Hadj <sabri.labonnevoix@gmail.com>
2025-09-12 10:05:45 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,1 @@
+const burger=document.querySelector('.burger');const nav=document.querySelector('.nav');if(burger&&nav){burger.addEventListener('click',()=>{const open=nav.classList.toggle('open');burger.setAttribute('aria-expanded',open?'true':'false');});}
